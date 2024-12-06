ac24ef76b0e93e54e5877b9c7623f10560e548cf
Jens Henske <Jens.Henske@outlook.com>
2024-12-06 08:27:15 +0100

    'autopush'

--- MSBuild.bat
+++ MSBuild.bat
@@ -1,15 +1,17 @@
 call "C:\Program Files (x86)\Embarcadero\Studio\23.0\bin\rsvars.bat"
 echo "Build erstellen"
 msbuild E:/Projekte/Windows/PCMManager/PCMManager.dproj /t:Clean;Build;CompressWin32 /p:config=Release /p:platform=Win32
-msbuild E:/Projekte/Windows/PCMManager/PCMManager.dproj /t:Clean;Build;Localize;CompressWin64 /p:config=Release /p:platform=Win64
+msbuild E:/Projekte/Windows/PCMManager/PCMManager.dproj /t:Clean;Build;CompressWin64;Localize /p:config=Release /p:platform=Win64
 
-echo "Kopiere Datei ins Setupverzeichnis"
-copy /y /v E:\Projekte\Windows\PCMManager\Win64\Release\PCMManager.exe "e:\Inno\Setupfiles\Programme\PCMManager_x64"
+echo "Kopiere Datei ins Setupverzeichnis 32-Bit"
 copy /y /v E:\Projekte\Windows\PCMManager\Win32\Release\PCMManager.exe "e:\Inno\Setupfiles\Programme\PCMManager"
 
+echo "Kopiere Datei ins Setupverzeichnis 64-Bit"
+copy /y /v E:\Projekte\Windows\PCMManager\Win64\Release\PCMManager.exe "e:\Inno\Setupfiles\Programme\PCMManager_x64"
 copy /y /v E:\Projekte\Windows\PCMManager\Win64\Release\PCMManager.DE "e:\Inno\Setupfiles\Programme\PCMManager_x64"
 copy /y /v E:\Projekte\Windows\PCMManager\Win64\Release\PCMManager.EN "e:\Inno\Setupfiles\Programme\PCMManager_x64"
 
+echo "Kopiere Doku ins Setupverzeichnis"
 copy /y /v E:\Projekte\Windows\PCMManager\PCMManager.docx "e:\Inno\Setupfiles\Programme\PCMManager"
 copy /y /v E:\Projekte\Windows\PCMManager\PCMManager.pdf "e:\Inno\Setupfiles\Programme\PCMManager"
 copy /y /v E:\Projekte\Windows\PCMManager\PCMManager.htm "e:\Inno\Setupfiles\Programme\PCMManager"
